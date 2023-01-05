c7b4dcf38bfc7201b251033bfe4e9d8577c6510b
Serg Ars <sergars@Vladyslavs-MacBook-Pro.local>
2023-01-05 15:52:09 +0200

    changed to process.cwd in  href in fetch by start

--- pages/index.js
+++ pages/index.js
@@ -188,20 +188,9 @@ export default function Home() {
 
 export async function getStaticProps(context) {
 
-  // const directory = path
-  // console.log('Object.keys(directory):', Object.keys(directory))
-  // console.log('directory.basename:', directory.basename)
-
-  // const processPWD = process.cwd()
-  // console.log('processPWD:', processPWD)
-  // console.log('typeof processPWD:', typeof processPWD)
-  //
-  // const postsDirectory = path.join(process.cwd(), 'posts')
-  // console.log('postsDirectory:', postsDirectory)
-
   const dev = process.env.NODE_ENV !== 'production';
-  const server = dev ? 'http://localhost:4003' : 'https://next-sqlite-crud-server.vercel.app';
-  // const server = dev ? 'http://localhost:4003' : `${process.cwd()}` ;
+  // const server = dev ? 'http://localhost:4003' : 'https://next-sqlite-crud-server.vercel.app';
+  const server = dev ? 'http://localhost:4003' : `${process.cwd()}` ;
 
   let requestOptions = {
     method: 'POST',
